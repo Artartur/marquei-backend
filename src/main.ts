1aaab68800447a94fe460ff0c0437d955e6f8eb0
artartur <arturgfs@hotmail.com>
2026-05-03 06:18:02 -0300

    feat(auth): define cookieOnly to refreshToken and enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { AppModule } from './app.module';
+import { configService } from './utils/configService';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL') ?? '',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
