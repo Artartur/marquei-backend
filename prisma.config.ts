75799ca2b130a03dfeb48bc0b87e230b0a6e1457
artartur <arturgfs@hotmail.com>
2026-05-03 01:22:33 -0300

    chore: initial settings

---
+++ prisma.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import 'dotenv/config';
+import { defineConfig } from 'prisma/config';
+
+const configService = new ConfigService();
+const dbUrl = configService.get<string>('DATABASE_URL');
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: dbUrl,
+  },
+});
