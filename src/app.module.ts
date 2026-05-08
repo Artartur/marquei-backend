5746ea95c6116b6e3456fe60e4dac14cbfc1b644
artartur <arturgfs@hotmail.com>
2026-05-08 15:23:56 -0300

    chore: add event-emitter e schedule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppointmentsModule } from './modules/appointments/appointments.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseModule } from './modules/database/database.module';
 import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 import { RolesGuard } from './modules/auth/guards/roles.guard';
+import { NotificationsModule } from './modules/notifications/notifications.module';
+import { ProfessionalsModule } from './modules/professionals/professionals.module';
 import { ServicesModule } from './modules/services/services.module';
 import { UsersModule } from './modules/users/users.module';
-import { ProfessionalsModule } from './modules/professionals/professionals.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    EventEmitterModule.forRoot(),
+    ScheduleModule.forRoot(),
     AppointmentsModule,
     AuthModule,
     DatabaseModule,
+    NotificationsModule,
     ProfessionalsModule,
     ServicesModule,
     UsersModule,
